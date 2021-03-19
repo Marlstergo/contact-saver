f23ba76dd096ea793abd5cd584e956f871e3ed96
Marlstergo <abdulmaliqshola@gmail.com>
2021-03-19 16:29:30 +0100

    header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
-
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
+import { checkUserSession, signOutUserStart } from '../../redux/user/user.action'
+import { selectCurrentUser } from '../../redux/user/user.selector'
 
 import './header.styles.scss'
 
-const Header = () =>{
+const Header = ({username, checkUserSession, logout}) =>{
 
-
+    useEffect(() =>{
+        checkUserSession()
+      }, [checkUserSession])
     return(
         <div className="header">
             <div className="header-container">
@@ -14,10 +18,10 @@ const Header = () =>{
                     <h1>CONTACT kEEPER</h1>
                 </div>
                 <div className="user-area">
-                    <span>Hello Name</span>
-                    <span>
+                    <span>Hellooo {username ? `${username.displayName}` : ''}</span>
+                    <span onClick={()=>logout()}>
                         <i className="fa fa-sign-out" aria-hidden="true"></i>
-                        Logout
+                        Logout now
                     </span>
                 </div>
 
@@ -26,4 +30,12 @@ const Header = () =>{
     )
 }
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+    username: selectCurrentUser
+})
+const mapDispatchToProps = dispatch => ({
+    logout : () => dispatch(signOutUserStart()),
+    checkUserSession: () => dispatch(checkUserSession())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
